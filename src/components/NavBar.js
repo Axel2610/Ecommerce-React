5ba021de966750dff7f660d1c8101fce31309bb3
Axel2610 <axelgarciamdp@hotmail.com>
2021-11-02 12:46:08 -0300

    PrimerCommit

---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+
+export const NavBar = () =>{
+    return(
+    <header>
+        <h1>Logo</h1>
+        <ul>
+            <li>Inicio</li>
+            <li>Productos</li>
+            <li>Contacto</li>
+        </ul>
+        <button className="botonUsuario">Usuario</button>
+    </header>
+    
+    )
+}
