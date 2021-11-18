51d2ba2ce78f69df5c859abe228d066f567c498e
Axel2610 <axelgarciamdp@hotmail.com>
2021-11-17 21:20:44 -0300

    clase6

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {NavBar} from "./components/Navbar/NavBar"
 import './App.css'
 import {ItemListContainer} from "./components/ItemListContainer/ItemListContainer"
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
---
+++ src/data/stock.js
@@ -0,0 +1,42 @@
+export const stock =[
+    {
+        id:'1',
+        name:'Producto 1',
+        price: 1000,
+        descrip: 'Lorem ipsum',
+        img: "https://picsum.photos/200",
+        stock: 10
+    },
+    {
+        id:'2',
+        name:'Producto 2',
+        price: 2000,
+        descrip: 'Lorem ipsum',
+        img: "https://picsum.photos/200",
+        stock: 15
+    },
+    {
+        id:'3',
+        name:'Producto 3',
+        price: 3000,
+        descrip: 'Lorem ipsum',
+        img: "https://picsum.photos/200",
+        stock: 20
+    },
+    {
+        id:'4',
+        name:'Producto 4',
+        price: 4000,
+        descrip: 'Lorem ipsum',
+        img: "https://picsum.photos/200",
+        stock: 25
+    },
+    {
+        id:'5',
+        name:'Producto 5',
+        price: 5000,
+        descrip: 'Lorem ipsum',
+        img: "https://picsum.photos/200",
+        stock: 30
+    }
+]
