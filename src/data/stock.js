64d9cf9cb636709806a0a1e47a4b6790bee86b2a
Axel2610 <axelgarciamdp@hotmail.com>
2021-11-18 12:02:35 -0300

    clase6

--- src/data/stock.js
+++ src/data/stock.js
@@ -1,42 +1,53 @@
+import notebook1 from '../img/intelCloudbook.jpg'
+import notebook2 from '../img/Hp-14-cf3028la.jpg'
+import notebook3 from '../img/Asus-X515.jpeg'
+import notebook4 from '../img/hp-15-corei5.jpg'
+import notebook5 from '../img/lenovo-v130.jpg'
+
 export const stock =[
     {
         id:'1',
-        name:'Producto 1',
-        price: 1000,
-        descrip: 'Lorem ipsum',
-        img: "https://picsum.photos/200",
-        stock: 10
+        name:'Intel CloudBook',
+        price: 53000,
+        descrip: 'La Cloudbook eNova NB142A-W10H posee: Una pantalla de 14.1" con resolución 1366 x 768 HD - Procesador Intel Celeron N3350 - Memoria RAM de 4 GM y Disco de 64 GB - Cámara Frontal - Sistema operativo Windows 10 - Wi-Fi - Bluetooth.',
+        img: notebook1,
+        stock: 10,
+        categoria: "Notebook"
     },
     {
         id:'2',
-        name:'Producto 2',
-        price: 2000,
-        descrip: 'Lorem ipsum',
-        img: "https://picsum.photos/200",
-        stock: 15
+        name:'HP 14',
+        price: 82000,
+        descrip: 'Diseño delgado y ultra portable para mantenerte conectado y con todas las tareas bajo control. Con batería duradera y pantalla de borde delgado la HP Laptop es tu compañera perfecta.',
+        img: notebook2,
+        stock: 15,
+        categoria: "Notebook"
     },
     {
         id:'3',
-        name:'Producto 3',
-        price: 3000,
-        descrip: 'Lorem ipsum',
-        img: "https://picsum.photos/200",
-        stock: 20
+        name:'Asus X515',
+        price: 78000,
+        descrip: ' Notebook Asus X515 Intel Core i3 4gb Ssd 256gb 15,6 Full Hd Ya sea para trabajar o jugar, el ASUS X515 es el computador portátil que ofrece un potente rendimiento y efectos visuales envolventes.',
+        img: notebook3,
+        stock: 20,
+        categoria: "Notebook"
     },
     {
         id:'4',
-        name:'Producto 4',
-        price: 4000,
-        descrip: 'Lorem ipsum',
-        img: "https://picsum.photos/200",
-        stock: 25
+        name:'HP 15',
+        price: 13000,
+        descrip: 'Manténgase concentrado y productivo con la ayuda de una computadora portátil HP 15-dw3025od. El generoso disco duro de 2TB le brinda mucho espacio para almacenar asignaciones, fotos, juegos, música y mucho más.',
+        img: notebook4,
+        stock: 25,
+        categoria: "Notebook"
     },
     {
         id:'5',
-        name:'Producto 5',
-        price: 5000,
-        descrip: 'Lorem ipsum',
-        img: "https://picsum.photos/200",
-        stock: 30
+        name:'Lenovo V130',
+        price: 63000,
+        descrip: 'La computadora portátil V130 de 15.6 " ofrece un gran rendimiento en una cubierta estampada y texturizada que muestra un estilo moderno. Un diseño simple y limpio presenta un panel táctil grande de una pieza.',
+        img: notebook5,
+        stock: 30,
+        categoria: "Notebook"
     }
 ]
