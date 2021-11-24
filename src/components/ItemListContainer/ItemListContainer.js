7dc21b7ff5ced7fbd83f999a95e1b979c250f54c
Axel2610 <axelgarciamdp@hotmail.com>
2021-11-23 23:45:30 -0300

    Update ItemListContainer.js

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import './ItemListContainer.css'
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useParams } from 'react-router'
 import { pedirDatos } from '../../helpers/pedirDatos'
 import { ItemList } from '../ItemList/ItemList'
